27ce52ff17e39fd9ab7c0b3fb0ab0f2f2c62a220
김세연 <seyeon@gimseyeon-ui-MacBookPro.local>
2026-02-27 19:25:58 +0900

    🐛 fromPhrase 형태 수정

--- src/utils/deriveWallet.ts
+++ src/utils/deriveWallet.ts
@@ -1,7 +1,7 @@
 import { HDNodeWallet } from 'ethers';
 
 export const deriveWallet = (mnemonic: string, index: number) => {
-  const rootWallet = HDNodeWallet.fromPhrase(mnemonic, "m/44'/60'/0'/0");
+  const rootWallet = HDNodeWallet.fromPhrase(mnemonic, undefined, "m/44'/60'/0'/0");
   const wallet = rootWallet.deriveChild(index);
   return {
     index,
